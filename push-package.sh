f92f29d2c6764289041596e971e2e8995b4bd4a8
beginor <beginor@qq.com>
2023-11-19 11:24:14 +0800

    update to .NET 8.0.0

--- push-package.sh
+++ push-package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-PACKAGE_VERSION="7.1.0"
+PACKAGE_VERSION="8.0.0"
 
 dotnet pack src/CustomHeader/CustomHeader.csproj -c Release
 dotnet nuget push src/CustomHeader/bin/Release/Beginor.AspNetCore.Middlewares.CustomHeader.$PACKAGE_VERSION.nupkg -s nuget.org -k $(cat ~/.nuget/key.txt)
